c3e57be6d574b6496acb0866df7f73fd317db378
Isabella Solarte <isabella32423@hotmail.com>
2023-11-11 21:21:35 -0500

    Login funcional

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<?php 
+
+  if (isset($_SESSION['user_id'])) {
+    header('Location: /php-login');
+  }
+  include("db.php");
+
+
+  if (!empty($_POST['user']) && !empty($_POST['password'])) {
+    $records = $conn->prepare('SELECT CC_USUARIO, LOGIN_USUARIO, CONTRASENIA_USUARIO FROM TRABAJADOR WHERE LOGIN_USUARIO = ?');
+    $records->bind_param('s', $_POST['user']);
+    $records->execute();
+    $records->bind_result($codTrabajador, $loginUsuario, $contraseniaUsuario);
+    $records->fetch();
+
+    if ($records->num_rows > 0 && password_verify($_POST['password'], $contraseniaUsuario)) {
+        $_SESSION['user_id'] = $codTrabajador;
+        header("Location: list_products.php");
+        exit();
+    } else {
+        $message = 'Sorry, those credentials do not match';
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,16 +32,16 @@
 </head>
 <body>
 	<div class="box">
-		<form autocomplete="off">
+		<form action="index.php" autocomplete="off" method="POST">
 			<h2>Sign in</h2>
 			<div class="inputBox">
-				<input type="text" required="required">
+				<input type="text" name="user" required="required">
 				<span>Usuario</span>
 				<i></i>
 			</div>
 			<div class="inputBox">
-				<input type="password" required="required">
-				<span>Contraseña</span>
+				<input type="password" name="password" required="required">
+				<span>Contraseña</span> 
 				<i></i>
 			</div>
 			<div class="links">
